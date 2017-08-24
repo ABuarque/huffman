ea4e83558f9255819db6af297a03d4d3671c9d24
arqui456 <arquimedes9137@hotmail.com>
2017-08-24 20:13:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void aplyHuffmand(char* filePath) {
+void applyHuffmand(char* filePath) {
 	printf("%s\n", filePath);
 }
 
 int main(int argc, char* argv[]) 
 {
 	if(argc >= 2) {
-		aplyHuffmand(argv[1]);
+		applyHuffmand(argv[1]);
 		return 0;
 	} else {
 		printf("Missing args!\nFinished with error!\n");
