4bcd558bf5452e31c2ab4fefdf4960ff2327c4bb
ABuarque <abuarquemf@gmail.com>
2017-10-05 16:12:45 -0300

    small changes

--- huffman/utils.h
+++ huffman/utils.h
@@ -1,8 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/***********************************************
+	Source file to add commom elements of use.
+*************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef unsigned char byte;
 
 #define ERR -1
+
+#define LOG_ERROR "LOG ERROR:"
 
 #endif
