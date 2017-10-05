ca3e065011c468563a4fb62d3af6d772aaa09a5a
ABuarque <abuarquemf@gmail.com>
2017-10-05 17:14:14 -0300

    added huffman handler files

---
+++ huffman/HuffmanHandler.c
@@ -0,0 +1,7 @@
+#include "HuffmanHandler.h"
+#include "HuffmanTree.h"
+#include "List.h"
+#include "utils.h"
+#include "PriorityQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
---
+++ huffman/HuffmanHandler.h
@@ -0,0 +1,24 @@
+#ifndef HUFFMANHANDLER_H
+#define HUFFMANHANDLER_H
+
+#include "HuffmanTree.h"
+#include "List.h"
+#include "utils.h"
+#include "PriorityQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/****************************************************
+ This source file actually handle Huffman'a algorithm.
+ It call the necessary data structures and aply the 
+ below processes: 
+
+-> get byte frequency
+-> build huffman tree
+-> destroy huffman tree
+-> compress
+-> decrompress
+*****************************************************/
+
+
+#endif
