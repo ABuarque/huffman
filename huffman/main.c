40e08c4acf4b82e7f148e3254827b5d75e12af4a
ABuarque <abuarquemf@gmail.com>
2017-10-05 15:53:06 -0300

    removed unecessary code

--- huffman/main.c
+++ huffman/main.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void applyHuffmand(char* filePath) {
-	printf("%s\n", filePath);
-}
-
-int main(int argc, char* argv[]) 
-{
-	if(argc >= 2) {
-		applyHuffmand(argv[1]);
-		return 0;
-	} else {
-		printf("Missing args!\nFinished with error!\n");
-		exit(1);
-	}
+int main(int argc, char* argv[]) {
+	
 }
