b9ad6d46d47ff59b6d7af103cca4b725c4d63679
ABuarque <abuarquemf@gmail.com>
2017-10-05 21:25:11 -0300

    added base entry point

--- huffman/HuffmanApplication.c
+++ huffman/HuffmanApplication.c
@@ -2,10 +2,20 @@
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define DEBUG if(1)
 
 void huffmanApplication(int argumentsCounter, char* argmentsVector[]) {
 	if(argumentsCounter == 4) {
-
+		if(!strcmp(argmentsVector[1], DO_COMPRESS)) { //if flag is -comp
+			DEBUG printf("dado -comp\n");
+		} else if(!strcmp(argmentsVector[1], DO_DECOMPRESS)) { //if is -decomp
+			DEBUG printf("dado -decomp\n");
+		} else { //anything else
+			printf("%s %s", LOG_ERROR, UNKNOW_ACTION_FLAG);
+			exit(1);
+		}
 	} else {
 		printf("%s %s", LOG_ERROR, BAD_ARGUMENTS);
 		exit(1);
