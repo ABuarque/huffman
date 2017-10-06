bae71d9630c6c1d35f2054d0bd2ecb8239a56cf1
ABuarque <abuarquemf@gmail.com>
2017-10-05 21:11:08 -0300

    updated

--- huffman/HuffmanApplication.c
+++ huffman/HuffmanApplication.c
@@ -1,6 +1,14 @@
 #include "HuffmanApplication.h"
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void huffmanApplication(int argumentsCounter, char* argmentsVector[]) {}
+void huffmanApplication(int argumentsCounter, char* argmentsVector[]) {
+	if(argumentsCounter == 4) {
+
+	} else {
+		printf("%s %s", LOG_ERROR, BAD_ARGUMENTS);
+		exit(1);
+	}
+}
 
--- huffman/HuffmanApplication.h
+++ huffman/HuffmanApplication.h
@@ -8,16 +8,27 @@ handles language preferences and user inputs,
 to compress or decompress some file.
 
 	On compress - cliente must provide these information flags:
-		* -comp: it says user wants to compress
+		* -comp: it says user wants to compress (action flag)
 		* FILE_PATH: it says where is the file
 		* OUTPUT_NAME: it says what is gonne be output name
 	On decrompress - cliente must provide these information flags
-		* -decomp: it says user wants to decompress
+		* -decomp: it says user wants to decompress (action flag)
 		* FILE_PATH: it says where is the file to be decompressed
 		* OUTPUT_NAME: it says what is gonne be output name
 		
 *****************************************************************/
 
+/**
+ * 	That's the entry point of Huffman app.
+ * It gets arguments sent on command line
+ * and runs the application with them.
+ *	From command line 4 arguments should
+ * be given: application launcher, action flag
+ * (see above description), input and output file.
+ * 
+ * @param number of arguments
+ * @param arguments list
+ */
 void huffmanApplication(int argumentsCounter, char* argmentsVector[]);
 
 #endif
