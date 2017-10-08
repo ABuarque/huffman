f683e43b7e52fdd080c6cb7005099ae42b1f32d2
ABuarque <abuarquemf@gmail.com>
2017-10-08 18:34:56 -0300

    added base implementation:

--- huffman/HuffmanHandler.c
+++ huffman/HuffmanHandler.c
@@ -5,3 +5,28 @@
 #include "PriorityQueue.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEBUG if(1)
+
+/****************************************************
+				Auxiliar functions
+	Client programmer doesn't need to take care
+about them, due to that, they've been separated from
+the interface.
+*****************************************************/
+
+
+/**********************************************************
+			Contract's functions imeplementation
+***********************************************************/
+void compressFile(char *inputPathFile, char *outputPathFile) {
+	DEBUG printf("INSIDE COMPRESS\n");
+}
+
+void decompressFile(char *inputPathFile, char *outputPathFile) {
+	DEBUG printf("INSIDE DECOMPRESS\n");
+}
+
+/**********************************************************
+			Auxiliar functions imeplementation
+**********************************************************/
