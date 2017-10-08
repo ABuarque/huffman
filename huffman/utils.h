b6d9182daf7a8edb73c7bb71419bc24760691594
ABuarque <abuarquemf@gmail.com>
2017-10-08 18:06:27 -0300

    removed unused constantes

--- huffman/utils.h
+++ huffman/utils.h
@@ -20,12 +20,6 @@ typedef unsigned char byte;
 //define a default constant to represent bad allocation error
 #define BAD_ALLOCATION "Failed to create new pointer.\n"
 
-//define a default constant to represent error caused by wron arguments
-#define BAD_ARGUMENTS "List arguments should be: ./app <ACTION_FLAG> <INPUT_FILE> <OUTPUT_FILE>\n"
-
-//define a default constant to alert about bugs caused by wrong flags
-#define UNKNOW_ACTION_FLAG "Invalid action flag values: -comp or -decomp\n"
-
 //define DO_COMPRESS as flag to run compress process
 #define DO_COMPRESS 1
 
