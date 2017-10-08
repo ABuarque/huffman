639e26f6b8bf77ffa8402cbe0d7c4c5ae41e3565
ABuarque <abuarquemf@gmail.com>
2017-10-08 16:03:32 -0300

    changeded app contract

--- huffman/HuffmanApplication.h
+++ huffman/HuffmanApplication.h
@@ -7,14 +7,12 @@
 handles language preferences and user inputs, 
 to compress or decompress some file.
 
-	On compress - cliente must provide these information flags:
-		* -comp: it says user wants to compress (action flag)
-		* FILE_PATH: it says where is the file
-		* OUTPUT_NAME: it says what is gonne be output name
-	On decrompress - cliente must provide these information flags
-		* -decomp: it says user wants to decompress (action flag)
-		* FILE_PATH: it says where is the file to be decompressed
-		* OUTPUT_NAME: it says what is gonne be output name
+	On compress - cliente must provide these information:
+		* Input file name (the file path)
+		* Ouput file name
+	On decrompress - cliente must provide these information
+		* Input file name (the file path)
+		* Ouput file name
 		
 *****************************************************************/
 
@@ -28,16 +26,9 @@ enum {EN = 1, PT, ES};
 
 /**
  * 	That's the entry point of Huffman app.
- * It gets arguments sent on command line
- * and runs the application with them.
- *	From command line 4 arguments should	
- * be given: application launcher, action flag
- * (see above description), input and output file.
  * 
- * @param number of arguments
- * @param arguments list
  */
-void huffmanApplication(int argumentsCounter, char* argmentsVector[]);
+void huffmanApplication();
 
 
 #endif
