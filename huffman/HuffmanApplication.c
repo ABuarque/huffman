a00b7fad256ca32d8cdeebc749c99564f01c87e1
ABuarque <abuarquemf@gmail.com>
2017-10-08 15:15:22 -0300

    added dependencies to handle support language and cosmecic frontend changes

--- huffman/HuffmanApplication.c
+++ huffman/HuffmanApplication.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#include <ctype.h>
 
 #define DEBUG if(1)
 
@@ -43,6 +45,7 @@ void huffmanApplication(int argumentsCounter, char* argmentsVector[]) {
 	header();
 	int chosenLanguage;
 	baseMenu(&chosenLanguage);
+	mainMenuHandler()
 
 }
 
