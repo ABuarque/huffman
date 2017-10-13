47bc1c087557388b920c4d758682b2dfab29d157
ABuarque <abuarquemf@gmail.com>
2017-10-12 22:27:20 -0300

    changed files reference

--- comparison_tests/insertion_plot.R
+++ comparison_tests/insertion_plot.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
-dadosNonHeap = read.table('teste.txt')
-dadosHeap= read.table('testeheap.txt')
+dadosNonHeap = read.table('priority_queue.csv')
+dadosHeap= read.table('heap_priority_queue.csv')
 plot (dadosNonHeap , main="Insertion on priority queue with lists" ,type="o"
       ,xlab="Insertion", ylab="Time taken", col="red")
 plot (dadosHeap , main="Insertion on priority queue with heap" ,type="o"
