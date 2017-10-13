92fefd7f7331943a6d2c2506df423f603e43632d
ABuarque <abuarquemf@gmail.com>
2017-10-12 22:50:13 -0300

    changed function to get csv files

--- comparison_tests/insertion_plot.R
+++ comparison_tests/insertion_plot.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
-dadosNonHeap = read.table('priority_queue.csv')
-dadosHeap= read.table('heap_priority_queue.csv')
+dadosNonHeap = read.csv('priority_queue.csv')
+dadosHeap= read.csv('heap_priority_queue.csv')
 plot (dadosNonHeap , main="Insertion on priority queue with lists" ,type="o"
       ,xlab="Insertion", ylab="Time taken", col="red")
 plot (dadosHeap , main="Insertion on priority queue with heap" ,type="o"
