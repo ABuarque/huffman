90bb84ef06a89adbf89d8d18c2c5603b1cf2667e
ABuarque <abuarquemf@gmail.com>
2017-10-29 03:23:15 -0300

    added commnets

--- huffman/Header.h
+++ huffman/Header.h
@@ -9,8 +9,21 @@ typedef struct {
 	byte treeSize;
 } Header;
 
+/**
+ * It gets scrap and size of tree
+ * and returns a struct with these values.
+ * 
+ * @param scrap
+ * @param tree size
+ * @return Header object
+ */
 Header* newHeader(byte scrap, byte treeSize);
 
+/**
+ * It gets a header object and destroys it.
+ * 
+ * @param header object
+ */
 void destroyHeader(Header* header);
 
 #endif
