481a9aac8732d8f22dd1a5c1fdc6c69e07b607c2
ABuarque <abuarquemf@gmail.com>
2017-11-02 19:08:53 -0300

    added color to app

--- huffman/utils.h
+++ huffman/utils.h
@@ -47,4 +47,18 @@ typedef unsigned char byte;
 //define a constant for string size
 #define SIZE_BUFFER 505
 
+//COLOR CODES AVAILABLE FOR LINUX BASED SYSTEM
+//TAKEN FROM https://ramprasadk.wordpress.com/2010/06/09/c-programming-linux-color-text-output/
+#define COLOR_RED   "\033[0;31m"        /* 0 -> normal ;  31 -> red */
+#define COLOR_CYAN  "\033[1;36m"        /* 1 -> bold ;  36 -> cyan */
+#define COLOR_GREEN "\033[4;32m"        /* 4 -> underline ;  32 -> green */
+#define COLOR_BLUE  "\033[9;34m"        /* 9 -> strike ;  34 -> blue */
+ 
+#define COLOR_BLACK  "\033[0;30m"
+#define COLOR_BROWN  "\033[0;33m"
+#define COLOR_MAGENTA  "\033[0;35m"
+#define COLOR_GRAY  "\033[0;37m"
+ 
+#define COLOR_NONE   "\033[0m"        /* to flush the previous property */
+
 #endif
