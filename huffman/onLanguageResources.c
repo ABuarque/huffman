6518afc5e4829ace6892658e3d5048ce1626bec1
ABuarque <abuarquemf@gmail.com>
2017-11-04 19:27:05 -0300

    separated language resources

---
+++ huffman/onLanguageResources.c
@@ -0,0 +1,26 @@
+#include "onLanguageResources.h"
+#include "utils.h"	
+
+void mainMenuPortuguese() {
+	printf("Digite uma das opções: \n");
+	printf("	(1) Codificar arquivo.\n"); 
+	printf("	(2) Decodificar.\n");
+	printf("	(3) Terminar programa.\n");
+	printf("	(4) Ver repositório no GitHub.\n");
+}
+
+void mainMenuEnglish() {
+	printf("Pick up an option: \n");
+	printf("	(1) Codify file.\n"); 
+	printf("	(2) Decode file.\n");
+	printf("	(3) End program.\n");
+	printf("	(4) See project on GitHub.\n");
+}
+
+void mainMenuEspanish() {
+	printf("Elija una de las opciones: \n");
+	printf("	(1) Codificar archivo.\n"); 
+	printf("	(2) Decodificar archivo.\n");
+	printf("	(3) Finalizar aplicación.\n");
+	printf("	(4) Ver proyecto en GitHub.\n");
+}
---
+++ huffman/onLanguageResources.h
@@ -0,0 +1,21 @@
+#ifndef ONLANGUAGERESOURCES_H
+#define ONLANGUAGERESOURCES_H
+
+#include "utils.h"
+
+/**
+ * Menu handle for portuguese language.
+ */
+void mainMenuPortuguese();
+
+/**
+ * Menu handle for english language.
+ */
+void mainMenuEnglish();
+
+/**
+ * Menu handle for spanish language.
+ */
+void mainMenuEspanish();
+
+#endif
