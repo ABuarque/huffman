db002221f8fdc8fd761b9d5f988b0045a875cc23
ABuarque <abuarquemf@gmail.com>
2017-11-04 19:31:28 -0300

    refactored frontend

--- huffman/HuffmanApplication.c
+++ huffman/HuffmanApplication.c
@@ -1,6 +1,7 @@
 #include "HuffmanApplication.h"
 #include "HuffmanHandler.h"
 #include "utils.h"
+#include "onLanguageResources.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,39 +47,6 @@ void baseMenu(int *chosen);
 void mainManu(void (*mainMenuHandler)(void));	
 
 /**
- * Menu handle for portuguese language.
- */
-void mainMenuPortuguese();
-
-/**
- * Menu handle for english language.
- */
-void mainMenuEnglish();
-
-/**
- * Menu handle for spanish language.
- */
-void mainMenuEspanish();
-
-/**
- * It asks user for the input file name
- * in portuguese.
- */
-void askForInputPortuguese();
-
-/**
- * It asks user for the input file name
- * in english.
- */
-void askForInputEnglish();
-
-/**
- * It asks user for the input file name
- * in espanish.
- */
-void askForInputEspanish(); 
-
-/**
  * Template function to ask user for input file name.
  *
  * @param a handle function.
@@ -86,54 +54,11 @@ void askForInputEspanish();
 void askForInput(void (*askForInputHandler)(void));
 
 /**
- * It asks user for the output file name
- * in english.
- */
-void askForOutputEnglish();
-
-/**
- * It asks user for the output file name
- * in portuguese.
- */
-void askForOutputPortuguese();
-
-/**
- * It asks user for the output file name
- * in espanish.
- */
-void askForOutputEspanish();
-
-/**
  * Template function to ask user for output file name.
  *
  * @param a handle function.
  */
 void askForOutput(void (*askForOutputHandler)(void));
-
-/**
- * It asks user for a valid input.
- * 
- * @param index of enum languages
- * @return propper message
- */
-char *askForValidInput(int index);
-
-/**
- * It says to user that the given input file name
- * is wrong.
- *
- * @param index enum language
- * @return a proper message
- */
-char *informWrongInputFileName(int index);
-
-/**
- * It says to user that extension file is not .huff.
- *
- * @param the index enum language
- * @return proper message
- */
-char* alertFileInvalidExtension(int index);
 
 //Array to store manu languages
 void (*preferenceLanguages[])(void) = { mainMenuEnglish, 
@@ -268,87 +193,14 @@ void openRepository() {
 	system("x-www-browser https://github.com/ABuarque/huffman");
 }
 
-void mainMenuPortuguese() {
-	printf("Digite uma das opções: \n");
-	printf("	(1) Codificar arquivo.\n"); 
-	printf("	(2) Decodificar.\n");
-	printf("	(3) Terminar programa.\n");
-	printf("	(4) Ver repositório no GitHub.\n");
-}
-
-void mainMenuEnglish() {
-	printf("Pick up an option: \n");
-	printf("	(1) Codify file.\n"); 
-	printf("	(2) Decode file.\n");
-	printf("	(3) End program.\n");
-	printf("	(4) See project on GitHub.\n");
-}
-
-void mainMenuEspanish() {
-	printf("Elija una de las opciones: \n");
-	printf("	(1) Codificar archivo.\n"); 
-	printf("	(2) Decodificar archivo.\n");
-	printf("	(3) Finalizar aplicación.\n");
-	printf("	(4) Ver proyecto en GitHub.\n");
-}
-
 void mainManu(void (*mainMenuHandler)(void)) {
 	mainMenuHandler();
-}
-
-void askForInputPortuguese() {
-	printf("Digite o nome do arquivo de entrada: ");
-}
-
-void askForInputEnglish() {
-	printf("Type input file name: ");
-}
-
-void askForInputEspanish() {
-	printf("Escriba el nombre de archivo de entrada: ");
 }
 
 void askForInput(void (*askForInputHandler)(void)) {
 	askForInputHandler();
 }
 
-void askForOutputEnglish() {
-	printf("Type desired output name: ");
-}
-
-void askForOutputPortuguese() {
-	printf("Digite o nome de saída desejado: ");
-}
-
-void askForOutputEspanish() {
-	printf("Escriba el nombre de salida desejado: ");
-}
-
 void askForOutput(void (*askForOutputHandler)(void)) {
 	askForOutputHandler();
-}
-
-char *askForValidInput(int index) {
-	printf("%s", COLOR_RED); //changin color warning to red
-	if(index == 0) 
-		return "Please type a valid value:\n";
-	else if(index == 1)
-		return "Por favor insira um argumento valido\n";
-	return "Per favor escriba un valor válido\n";
-}
-
-char *informWrongInputFileName(int index) {
-	if(index == 0)
-		return "Impossible to find file with given name, try again: ";
-	else if(index == 1)
-		return "Impossível encontrar arquivo com nome informado, tente de novo: ";
-	return "Imposible encontrar un archivo con un nombre determinado, escriba novamente: ";
-}
-
-char* alertFileInvalidExtension(int index) {
-	if(index == 0)
-		return "Given file has not right extension: '.huff', Please, type it again.\n";
-	else if(index == 1)
-		return "Arquivo informado não possui extensão correta: '.huff', Por favor, digite de novo.\n";
-	return "El archivo dado no tiene la extensión correcta: '.huff', por favor, tipea de nuevo.\n";
 }
