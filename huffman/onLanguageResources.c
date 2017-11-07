d027729d333029d90d76d6bd86d07b378361b6f1
ABuarque <abuarquemf@gmail.com>
2017-11-07 03:23:42 -0300

    fixed wrong words

--- huffman/onLanguageResources.c
+++ huffman/onLanguageResources.c
@@ -3,24 +3,24 @@
 
 void mainMenuPortuguese() {
 	printf("Digite uma das opções: \n");
-	printf("	(1) Codificar arquivo.\n"); 
-	printf("	(2) Decodificar.\n");
+	printf("	(1) Comprimir arquivo.\n"); 
+	printf("	(2) Descomprimir.\n");
 	printf("	(3) Terminar programa.\n");
 	printf("	(4) Ver repositório no GitHub.\n");
 }
 
 void mainMenuEnglish() {
 	printf("Pick up an option: \n");
-	printf("	(1) Codify file.\n"); 
-	printf("	(2) Decode file.\n");
+	printf("	(1) Compress file.\n"); 
+	printf("	(2) Decompress file.\n");
 	printf("	(3) End program.\n");
 	printf("	(4) See project on GitHub.\n");
 }
 
 void mainMenuEspanish() {
 	printf("Elija una de las opciones: \n");
-	printf("	(1) Codificar archivo.\n"); 
-	printf("	(2) Decodificar archivo.\n");
+	printf("	(1) Comprimir archivo.\n"); 
+	printf("	(2) Descomprimir archivo.\n");
 	printf("	(3) Finalizar aplicación.\n");
 	printf("	(4) Ver proyecto en GitHub.\n");
 }
